a331e8d8e5e09794a1d38652bd5ff764f7bd385e
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-06-06 12:19:32 -0500

    Modificaciones HTML, inicio func. JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,13 @@
 window.cipher = {
   // ... 
+  encode: function(ofset,string){
+    console.log('encode');
+
+    return variable;
+
+  },
+  decode: function(ofset,string){
+    console.log('decode');
+
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log ("hola mundo")
+
+document.getElementById('pagina1').style.display = 'block';
+
+function cifrar(){
+    document.getElementById('pagina1').style.display = 'none';
+    document.getElementById('pagina2').style.display = 'block';
+    document.getElementById('pagina3').style.display = 'none';
+    document.getElementById('pagina4').style.display = 'none'; 
+}  
+function decifrar(){
+    document.getElementById('pagina1').style.display = 'none';
+    document.getElementById('pagina2').style.display = 'none';
+    document.getElementById('pagina3').style.display = 'block';
+    document.getElementById('pagina4').style.display = 'none'; 
+}
+function resultadoCifrar(){
+    var ofset=document.getElementById('numberCifrar').value;
+    var string=document.getElementById('textoAcifrar').value;
+    var codificado = window.cipher.encode(ofset,string);
+    document.getElementById('pagina1').style.display = 'none';
+    document.getElementById('pagina2').style.display = 'none';
+    document.getElementById('pagina3').style.display = 'none';
+    document.getElementById('pagina4').style.display = 'block';
+
+}
+function resultadoDecifrar(){
+    var ofset=document.getElementById('numberDecifrar').value;
+    var string=document.getElementById('textoAdecifrar').value;
+    window.cipher.decode(ofset,string);
+    document.getElementById('pagina1').style.display = 'none';
+    document.getElementById('pagina2').style.display = 'none';
+    document.getElementById('pagina3').style.display = 'none';
+    document.getElementById('pagina4').style.display = 'block';
+
+}
