147781778c3e7619ee14f7f84d37d9982368a2ba
LauraMayaVayaHayaRaya <lauramaya86@gmail.com>
2019-06-14 08:51:52 -0500

    sustituir funciones y onclicks

--- src/cipher.js
+++ src/cipher.js
@@ -19,7 +19,7 @@ window.cipher = {
     for(let i=0;i< string.length;i++){
       let ascii=string[i].toUpperCase().charCodeAt();
       if (ascii >= 65 && ascii <= 90){
-        let saltos=(ascii-65-ofset)%26+65;
+        let saltos=(ascii+65-ofset)%26+65; //minúsculas --+
         palabraDecifrada+=String.fromCharCode(saltos);
       }else{
         palabraDecifrada+=String.fromCharCode(ascii);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 
 document.getElementById('pagina1').style.display = 'block';
 
-function cifrar(){
+//function cifrar(){
+ const cifrar=()=>{
     document.getElementById('pagina1').style.display = 'none';
     document.getElementById('pagina2').style.display = 'block';
     document.getElementById('pagina3').style.display = 'none';
     document.getElementById('pagina4').style.display = 'none'; 
-}  
-function decifrar(){
+}; 
+let linkCifrar = document.getElementById("cifrar");
+linkCifrar.addEventListener("click", cifrar);
+let linkRegresarACifrar = document.getElementById("regresarAcifrar");
+linkRegresarACifrar.addEventListener("click", cifrar);
+
+
+
+//function decifrar(){
+    const decifrar=()=>{
     document.getElementById('pagina1').style.display = 'none';
     document.getElementById('pagina2').style.display = 'none';
     document.getElementById('pagina3').style.display = 'block';
     document.getElementById('pagina4').style.display = 'none'; 
-}
-function resultadoCifrar(){
+};
+let linkDecifrar = document.getElementById("decifrar");
+linkDecifrar.addEventListener("click", decifrar);
+let linkRegresarADecifrar = document.getElementById("regresarAdecifrar");
+linkRegresarADecifrar.addEventListener("click", decifrar);
+
+
+//function resultadoCifrar(){
+    const resultadoCifrar=()=>{
     let ofset=parseInt(document.getElementById('numberCifrar').value);
     let string=document.getElementById('textoAcifrar').value;
     let encode=window.cipher.encode(ofset,string);//guarda el resultado que me da la funcion encode
@@ -23,8 +39,12 @@ function resultadoCifrar(){
     document.getElementById('pagina4').style.display = 'block';
     document.getElementById('resultados').innerHTML=encode;
 
-}
-function resultadoDecifrar(){
+};
+let linkResultadoCifrar = document.getElementById("botonResultadoCifrar");
+linkResultadoCifrar.addEventListener("click", resultadoCifrar);
+
+//function resultadoDecifrar(){
+    const resultadoDecifrar=()=>{
     let ofset=document.getElementById('numberDecifrar').value;
     let string=document.getElementById('textoAdecifrar').value;
     let decode=window.cipher.decode(ofset,string);
@@ -34,4 +54,7 @@ function resultadoDecifrar(){
     document.getElementById('pagina4').style.display = 'block';
     document.getElementById('resultados').innerHTML=decode;
 
-}
+};
+let linkResultadoDecifrar = document.getElementById("botonResultadoDecifrar");
+linkResultadoDecifrar.addEventListener("click", resultadoDecifrar);
+
